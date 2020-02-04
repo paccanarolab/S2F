f46ae2f221f41995fc430558e4a718439a4766ed
Mateo Torres <torresmateo@gmail.com>
2020-02-04 14:29:07 -0300

    bugfix: implemented worker function

--- commands/RescoreContinuous.py
+++ commands/RescoreContinuous.py
@@ -6,7 +6,28 @@ import pandas as pd
 import numpy as np
 import os
 
-def filter_predictions_worker(df, th, protein):
+def filter_predictions_worker(df, th, protein, go):
+    df.to_csv('test.df', sep='\t', header=None)
+    data = {'protein':[], 'go_term':[], 'score':[]}
+    term_cache = set()
+    for go_term in df.go_term.unique():
+        term = go.find_term(go_term)
+        score = df[df.go_term == go_term].at[0, 'score']
+        children = term.get_children()
+        keep = True
+        for c in children:
+            if len(df[df.go_term == c.name]) < 1:
+                continue
+            c_score = df[df.go_term == c.name].at[0, 'score']
+            if c_score <= score + th:
+                keep = False
+                break
+        if keep:
+            data['protein'].append(protein)
+            data['go_term'].append(go_term)
+            data['score'].append(score)
+    res = pd.DataFrame(data)
+    return res
 
 class RescoreContinuous(FancyApp.FancyApp):
 
@@ -59,7 +80,7 @@ class RescoreContinuous(FancyApp.FancyApp):
         for protein in proteins:
             predictions = prediction_df[prediction_df.protein == protein]['go_term', 'score']
             th = self.th_start
-            params.append([predictions, th, protein])
+            params.append([predictions, th, protein, self.go])
             if self.__verbose__:
                 prog.increment_amount()
                 prog.print_bar()
@@ -67,7 +88,7 @@ class RescoreContinuous(FancyApp.FancyApp):
             prog.finish_bar()
         self.tell(f'Filtering DataFrames using {self.cpu} cores')
         with Pool(self.cpu) as p:
-            p.starmap(params)
+            p.starmap(params, filter_predictions_worker)
         
         outfile = os.join(self.outdir, os.path.basename(self.prediction) + '_filtered')
         self.tell('Saving prediction to file...')
