98888ad5d29af9623dcee7740c1e65aac92fab43
Mateo Torres <torresmateo@gmail.com>
2020-04-02 12:42:42 -0300

    bugfix

--- bacteria_selection.py
+++ bacteria_selection.py
@@ -128,7 +128,7 @@ else:
     selection_count_df = pd.DataFrame(data)
     selection_count_df.to_pickle(DATA_DIRECTORY+'selection_count_df.pkl')
 
-pickle.dump(data, os.path.join(DATA_DIRECTORY,'data.pkl'))
+pickle.dump(data, open(os.path.join(DATA_DIRECTORY,'data.pkl'), 'wb'))
 selection_df = bacteria_df.merge(selection_count_df, left_on='Tax ID', right_on='Tax ID')
 
 ontology_tau = 8
